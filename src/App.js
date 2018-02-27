a87a69aa1fa683ded4e72ed747899aa80e744458
Kevin Budd <kevin@kevinbudd.com>
2018-02-27 13:48:34 -0600

    re-commit with Album.js filename capitalized

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
-import Library from './components/Library';
 import Album from './components/Album';
+import Library from './components/Library';
+
 
 class App extends Component {
   render() {
